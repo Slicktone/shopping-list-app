353060a91a084d834182650b2a475f0ac4af8e93
Slicktone <tclaytony23@gmail.com>
2015-09-29 23:12:15 -0700

    Javascript and HTML updates

---
+++ js/app.js
@@ -0,0 +1,27 @@
+//jquery way
+/*$(document).ready(function() {
+	$('.button').click(function() {
+		if ($('input').val().length > 0{
+			var value = $('input').val();
+			$('#inputBar').append()
+
+		}
+	}
+}*/
+//javascript way
+var jsObj= {};
+var jsArray = [];
+
+var ArrayOfObj = [{}, {}, {}];
+
+var ObjOfArrays = {"Key1":[], "Key2":[]};
+
+shoppingList = ["an", "array", "of", "things"];
+
+$("li").length;
+function onButtonClick() {
+	shoppingList.push ({
+		name: $('#inputBar').value(),
+	});
+
+});
